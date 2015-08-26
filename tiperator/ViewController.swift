d38101ec4d3a71317901392a99d71b25c77f905d
Laci White <laciwhite@Lacis-MacBook-Air.local>
2015-08-25 20:48:58 -0700

    Initial Commit

---
+++ tiperator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tiperator
+//
+//  Created by Laci White on 8/25/15.
+//  Copyright (c) 2015 Laci White. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
